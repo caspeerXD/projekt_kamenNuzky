1a10238dc9006523f2f09e49f4e6d9c9660484f1
Jiří Hornych <jiri.hornych@educhem.cz>
2022-06-24 07:39:06 +0200

    Přidejte soubory projektu.

---
+++ projekt_kamenNuzky/Hra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaverecny_projekt
+{
+    internal class Hra
+    {
+        public int SkoreHrac { get; set; }
+        public int SkoreProtiHrac { get; set; }
+        public Random generatorVoleb = new Random();
+
+
+        public Hra()
+        {
+            SkoreHrac = 0;
+            SkoreProtiHrac = 0;
+        }
+
+    }
+}
